26d15c4dce9a534c734626b136a28fd44395d70b
Hanz <haniel56@zoho.eu>
2023-07-10 03:53:01 +0530

    feat: static serve from sveltekit build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
+    Router, Server,
+};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    // declare all routes
+    let routes: Router = Router::new()
+        .route("/healthz", get(hello_handler))
+        .nest_service("/", get_service(ServeDir::new("./web/build/")));
+
+    // ip address and port
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {}", addr); // ! convert to tracing
+
+    // start server
+    Server::bind(&addr)
+        .serve(routes.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_handler() -> impl IntoResponse {
+    Html("<h1>Hello, from the other side!</h1>") // ! convert it to health check
+}
+
+async fn static_web_router() -> Router {
+    todo!("add static web router fn")
 }
