85aaf62ce21fb733544de7317ae5de946d40ff8a
Hanz <haniel56@zoho.eu>
2023-07-11 20:23:08 +0530

    chore: authenticate forms

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -5,10 +5,20 @@
   import '@skeletonlabs/skeleton/styles/skeleton.css';
   // Finally, your application's global stylesheet (sometimes labeled 'app.css')
   import '../app.postcss';
-
+  // authorizer
+	import '@authorizerdev/authorizer-svelte/styles/default.css';
+  import { AuthorizerProvider } from '@authorizerdev/authorizer-svelte';
+  // skeleton ui components
   import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
+<AuthorizerProvider
+    config={{
+        authorizerURL: `https://d-auth.hanz.lol/`,
+        redirectURL: typeof window != 'undefined' ? window.location.origin : ``,
+        client_id: 'b221ab06-c89f-4c40-a1da-34fac30ca7fc',
+    }}
+>
 <AppShell>
   <!-- header -->
 	<svelte:fragment slot="header">
@@ -17,7 +27,7 @@
       <svelte:fragment slot="lead">
         <h2 class="h2">dash.hanz.lol</h2>
       </svelte:fragment>
-
+      
       <!-- right side -->
       <svelte:fragment slot="trail">
         <div class="flex gap-4 align-baseline">
@@ -30,11 +40,11 @@
     </AppBar>
   </svelte:fragment>
   <!-- end header -->
-
+  
   <!-- body -->
   <slot />
   <!-- end body -->
-
+  
   <!-- footer -->
   <svelte:fragment slot="pageFooter">
     <div class="h-18 variant-soft-surface p-4">
@@ -43,3 +53,4 @@
   </svelte:fragment>
   <!-- end footer -->
 </AppShell>
+</AuthorizerProvider>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,3 +1,37 @@
-<div class="p-4">
-    <p>Hello world</p>
+<script lang="ts">
+    import { getContext } from 'svelte';
+    import { Authorizer } from '@authorizerdev/authorizer-svelte';
+
+    /**
+	 * @type {{ token: string; user: any; loading: boolean; logout: Function; }}
+	 */
+	let state: { token: string; user: any; loading: boolean; logout: Function; };
+    
+    const store: any = getContext('authorizerContext');
+    store.subscribe((/** @type {any} */ data: any) => {
+		state = data;
+	});
+
+	const logoutHandler = async () => {
+		await state.logout();
+	};
+</script>
+
+<div class="px-4 max-w-lg flex flex-col items-center justify-center mx-auto gap-8 min-h-screen ">
+    {#if state.user}
+        <div class="flex gap-4">
+            <h1 class="h1">Hey 👋,</h1>
+            <span>{state.user.email}</span>
+
+            {#if state.loading}
+                <h3 class="h3">Processing....</h3>
+            {:else}
+                <!-- <h3 class="h3" on:click={logoutHandler}>Logout</h3> -->
+                <h3 class="h3">Logout</h3>
+            {/if}
+        </div>
+    {:else}
+        <h1 class="h1">authenticate</h1>
+        <Authorizer />
+    {/if}
 </div>
