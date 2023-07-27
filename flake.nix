0af9b4f45fa8beccb23ffb445d5e260470b1cce6
Hanz <haniel56@zoho.eu>
2023-07-27 19:24:47 +0530

    chore: flake fmt w/ alejandro

--- flake.nix
+++ flake.nix
@@ -7,36 +7,31 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , rust-overlay
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [
         (import rust-overlay)
         (self: super: rec {
           nodejs = super.nodejs-18_x;
-          yarn = (super.yarn.override { inherit nodejs; });
-          rustToolchain =
-            let
-              rust = super.rust-bin;
-            in
-            if builtins.pathExists ./rust-toolchain.toml then
-              rust.fromRustupToolchainFile ./rust-toolchain.toml
-            else if builtins.pathExists ./rust-toolchain then
-              rust.fromRustupToolchainFile ./rust-toolchain
-            else
-              rust.stable.latest.default; 
+          yarn = super.yarn.override {inherit nodejs;};
+          rustToolchain = let
+            rust = super.rust-bin;
+          in
+            if builtins.pathExists ./rust-toolchain.toml
+            then rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain
+            then rust.fromRustupToolchainFile ./rust-toolchain
+            else rust.stable.latest.default;
         })
       ];
 
-      pkgs = import nixpkgs { inherit system overlays; };
-    in
-    {
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           rustToolchain
@@ -47,7 +42,7 @@
           cargo-watch
           clippy
           diesel-cli
-          
+
           node2nix
           nodejs
           yarn
