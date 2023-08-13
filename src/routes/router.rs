4ba65cfc6034eec11e3a2c1f888329a4e03fe058
Hanz <haniel56@zoho.eu>
2023-08-14 05:29:34 +0530

    chore: create and read works

--- src/routes/router.rs
+++ src/routes/router.rs
@@ -1,4 +1,6 @@
+use crate::db::user::User;
 use axum::{
+    extract::Path,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, get_service, MethodRouter},
@@ -6,7 +8,8 @@ use axum::{
 };
 use tower_http::services::ServeDir;
 use tower_http::trace::{self, TraceLayer};
-use tracing::Level;
+use tracing::{debug, warn, Level};
+use uuid::Uuid;
 
 /// Creates a router for handling HTTP requests.
 pub fn create_router() -> Router {
@@ -24,6 +27,8 @@ pub fn create_router() -> Router {
         .fallback_service(static_router_service)
         // .nest_service("/", static_router_service) // static web router
         .route("/healthz", get(health_check_handler)) // health check
+        .route("/create_user", get(create_user))
+        .route("/read_user/:id", get(read_user))
         // .fallback(not_found_handler)
         .layer(http_trace_layer);
 
@@ -38,4 +43,38 @@ async fn health_check_handler() -> impl IntoResponse {
 /// Handler for not found endpoint.
 async fn not_found_handler() -> impl IntoResponse {
     Json("oh no! 404 not found.")
+}
+
+// test create user router
+async fn create_user() -> impl IntoResponse {
+    match User::new(
+        "example@email.com".to_string(),
+        "password".to_string(),
+        "hanz".to_string(),
+        "Haniel".to_string(),
+    )
+    .await
+    {
+        Ok(user) => {
+            debug!("processed create_user request {:#?}", &user);
+            Ok(Json(user))
+        }
+        Err(e) => {
+            warn!("failed to process create_user, {:#?}", &e);
+            Err((StatusCode::BAD_REQUEST, e.to_string()))
+        }
+    }
+}
+
+async fn read_user(Path(id): Path<Uuid>) -> impl IntoResponse {
+    match User::get_user(id).await {
+        Some(user) => {
+            debug!("processed get_user request {:#?}", &user);
+            Ok(Json(user))
+        }
+        None => {
+            warn!("failed to process get_user, {:?}", &id);
+            Err((StatusCode::BAD_REQUEST, Json("user not found")))
+        }
+    }
 }
