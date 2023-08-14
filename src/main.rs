356ca42879706f19652de1921808a545ed2dc61d
Hanz <haniel56@zoho.eu>
2023-08-15 04:58:14 +0530

    chore: refactor and improvements

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod configs;
 mod db;
 mod routes;
 
+use crate::db::connection::Connection;
 use axum::Server;
 use configs::Configs;
 use std::net::SocketAddr;
@@ -10,27 +11,25 @@ use tracing::{debug, info};
 #[tokio::main]
 async fn main() {
     // initialize tracing subscriber
-    tracing_subscriber::fmt().finish();
-
-    // initialize environment configs
-    let config: Configs = Configs::init_configs();
-
-    // finalize tracing subscriber
-    tracing_subscriber::fmt()
+    let _subscriber = tracing_subscriber::fmt()
         .compact()
         .with_target(false)
         .with_file(true)
         .with_line_number(true)
-        .with_max_level(config.log_level())
+        .with_max_level(tracing::Level::DEBUG)
         .init();
+    // ! configure tracing to start with default
+    // ! until config.loglevel becomes available
 
-    // log configs
+    // initialize environment configs
+    let config = Configs::init();
     debug!("{:?}", &config);
 
+    // finalize tracing subscriber
+    // subscriber.with_max_level(config.log_level()).init();
+
     // initialize database connection
-    let db: Result<surrealdb::opt::auth::Jwt, surrealdb::Error> =
-        db::connection::init(config.clone()).await;
-    debug!("{:?}", &db);
+    let _db = Connection::new().init(&config).await;
 
     // ip address and port
     let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], config.port));
