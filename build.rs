db109eb7020a3771688bff168dca33fae622033a
Hanz <haniel56@zoho.eu>
2023-08-22 15:42:24 +0530

    feat: user CRUD w/ surrealdb

--- build.rs
+++ build.rs
@@ -4,8 +4,7 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .type_attribute(".", "#[derive(serde::Serialize)]")
-        .type_attribute(".", "#[derive(serde::Deserialize)]")
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(
             PathBuf::from(env::var("OUT_DIR").unwrap()).join("api_descriptor.bin"),
